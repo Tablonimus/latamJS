178ee9b509c2604a5fa08e13a38c93c383f61843
tablonimus <lautamartin1995@gmail.com>
2023-06-02 01:08:52 -0300

    nuevo}

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,36 +2,37 @@ const createCard = document.getElementById("createCard");
 const root = document.getElementById("root");
 const divContador = document.getElementById("contador");
 
-let contador = 1;
 let colorElegido = "";
+let contador = 0; //
 
 createCard.addEventListener("click", (event) => {
   let nombre = prompt("escriba su nombre");
   contador = contador + 1;
 
-  root.innerHTML += `    
-    <div id="productCard" class="nuevo"> 
-         <button name=${contador} id="delete">❌</button>
+  root.innerHTML = `
+  <div id=${nombre}>    
+    <div id="productCard" class="nuevo" style="background-color:${colorElegido}"> 
+         <button name=${nombre} id=${nombre+1}>❌</button>
          <img class="product" src="https://images.unsplash.com/photo-1607870379000-1f6d5e892aba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1452&q=80">
          <p>${nombre}</p>
 
-    </div>`;
+    </div>
+  </div>
+    `;
+
   divContador.innerHTML = contador;
 
-  const deleteButton = document.getElementById("delete");
+  const deleteButton = document.getElementById(`${nombre}`);
+  console.log(deleteButton);
   deleteButton.addEventListener("click", (event) => {
     const aEliminar = document.getElementById(`${event.target.name}`);
     aEliminar.innerHTML = "";
+    contador = contador - 1;
+
+    contador === 0
+      ? (divContador.innerHTML = "")
+      : (divContador.innerHTML = contador);
   });
-  console.log(deleteButton);
-});
-
-const colorSwitch = document.getElementById("changeColor");
-colorSwitch.addEventListener("click", (event) => {
-  let color = prompt("elige un color");
-
-  const productCard = document.getElementById("productCard");
-  productCard.style.backgroundColor = color;
 });
 
 const colorInput = document.getElementById("colorInput");
